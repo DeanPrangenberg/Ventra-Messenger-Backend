2ae86f4e9f42b2839e3c81693d82aa738a01c3d2
DeanPrangenberg <prangenbergdean@gmail.com>
2025-06-05 19:46:53 +0200

    Updated code structure

---
+++ QTClient/src/Logic/DataBaseOperations/UserDataDB.cpp
@@ -0,0 +1,30 @@
+//
+// Created by deanprangenberg on 28.05.25.
+//
+
+#include "UserDataDB.h"
+
+namespace logic {
+  UserDataDB::UserDataDB(const fs::path &dbPath, const std::string &password, bool debugMode)
+      : LocalDatabase(dbPath, password, debugMode) {
+    if (createUserTables()) {
+      std::cout << "User tables created successfully" << std::endl;
+      if (debugMode) {
+        std::cout << "Running in DEBUG mode (unencrypted database)" << std::endl;
+      }
+    } else {
+      std::cerr << "Error creating user tables: " << getLastError() << std::endl;
+    }
+  }
+
+  bool UserDataDB::createUserTables() {
+    std::string createUsersTable =
+        "CREATE TABLE IF NOT EXISTS users ("
+        "user_uuid TEXT PRIMARY KEY,"
+        "username TEXT NOT NULL,"w
+        "avatar BLOB"
+        ");";
+
+    return execute(createUsersTable);
+  }
+}
