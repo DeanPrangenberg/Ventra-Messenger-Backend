200ce26320145b6799737b2f4cc8a93d39727f0c
DeanPrangenberg <prangenbergdean@gmail.com>
2025-07-25 12:41:36 +0200

    Refined protoc script: output position

--- Backend/shared/Redis-API-Wrapper/src/Init.go
+++ Backend/shared/Redis-API-Wrapper/src/Init.go
@@ -1,17 +1,1 @@
 package RedisWrapper
-
-import (
-	"context"
-	"fmt"
-	"github.com/redis/go-redis/v9"
-)
-
-func (rc *RedisClient) initRedisClient(cfg Config) {
-	ctx, cancel := context.WithCancel(context.Background())
-	client := &RedisClient{
-		mode:   cfg.Mode,
-		topic:  cfg.Topic,
-		ctx:    ctx,
-		cancel: cancel,
-	}
-}
